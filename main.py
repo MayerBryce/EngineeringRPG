e7137904ebec7bf56d99a9f4586dd1e0226a2b8a
Ivan <rocketspongebob@yahoo.com>
2021-04-29 14:07:22 -0400

    added remaining menus + corresponding battle logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 from characters import *
 from menus import *
 
@@ -34,9 +35,9 @@ if selectionNum == '5':
 firstMenu()
 input()
 #os.system('clear')
-secondMenu()
 
-#BEGIN MINI BOSS FIGHT
+
+#BEGIN MINI BOSS FIGHT          (maybe??) change naming conventions (academic advisor is the mini boss) OR we can have every enemy encounter except for the final boss be considered a mini boss; would be easier than changing the name of each variables
 annoyingStudent = MiniBoss()
 while annoyingStudent.hp > 0:
     #os.system('clear')
@@ -45,6 +46,45 @@ while annoyingStudent.hp > 0:
     if selectionNum == '1':
         annoyingStudent.hp = annoyingStudent.hp - playerClass.attack
     playerClass.hp = playerClass.hp - annoyingStudent.attack
+    if selectionNum == '2':
+                finalBoss.hp = finalBoss.hp - playerClass.diningDollars
+                playerClass.diningDollars -= 50
+    if playerClass.hp <= 0:
+        #os.system('clear')
+        #lostToMiniBoss()
+        quit()
+playerClass.level += 1
+victoryMiniBoss()
+
+cleaningJanitor = MiniBoss()
+while cleaningJanitor.hp > 0:
+    #os.system('clear')
+    miniBossFight(playerClass.hp,cleaningJanitor.hp,playerClass.level)
+    selectionNum = input()
+    if selectionNum == '1':
+        cleaningJanitor.hp = cleaningJanitor.hp - playerClass.attack
+    playerClass.hp = playerClass.hp - cleaningJanitor.attack
+    if selectionNum == '2':
+                finalBoss.hp = finalBoss.hp - playerClass.diningDollars
+                playerClass.diningDollars -= 50
+    if playerClass.hp <= 0:
+        #os.system('clear')
+        #lostToMiniBoss()
+        quit()
+playerClass.level += 1
+victoryMiniBoss()
+
+academicAdvisor = MiniBoss()
+while academicAdvisor.hp > 0:
+    #os.system('clear')
+    miniBossFight(playerClass.hp,academicAdvisor.hp,playerClass.level)
+    selectionNum = input()
+    if selectionNum == '1':
+        academicAdvisor.hp = academicAdvisor.hp - playerClass.attack
+    playerClass.hp = playerClass.hp - academicAdvisor.attack
+    if selectionNum == '2':
+                finalBoss.hp = finalBoss.hp - playerClass.diningDollars
+                playerClass.diningDollars -= 50
     if playerClass.hp <= 0:
         #os.system('clear')
         #lostToMiniBoss()
@@ -53,3 +93,82 @@ playerClass.level += 1
 victoryMiniBoss()
 
 #BEGIN TRANSITION TO BOSS FIGHT
+
+for x in range(1):
+    whichBoss = random.randint(1, 4)
+    if whichBoss == 1:
+        finalBoss = Klenke() 
+        while finalBoss.hp > 0:
+            #os.system('clear')
+            miniBossFight(playerClass.hp,finalBoss.hp,playerClass.level)
+            selectionNum = input()
+            if selectionNum == '1':
+                finalBoss.hp = finalBoss.hp - playerClass.attack
+            playerClass.hp = playerClass.hp - finalBoss.attack
+            if selectionNum == '2':
+                finalBoss.hp = finalBoss.hp - playerClass.diningDollars
+                playerClass.diningDollars -= 50
+            if playerClass.hp <= 0:
+                #os.system('clear')
+                #lostToMiniBoss()
+                quit()
+        playerClass.level += 1
+        victoryFinalBoss()
+
+    elif whichBoss == 2:
+        finalBoss = Motai() 
+        while finalBoss.hp > 0:
+            #os.system('clear')
+            miniBossFight(playerClass.hp,finalBoss.hp,playerClass.level)
+            selectionNum = input()
+            if selectionNum == '1':
+                finalBoss.hp = finalBoss.hp - playerClass.attack
+            playerClass.hp = playerClass.hp - finalBoss.attack
+            if selectionNum == '2':
+                finalBoss.hp = finalBoss.hp - playerClass.diningDollars
+                playerClass.diningDollars -= 50
+            if playerClass.hp <= 0:
+                #os.system('clear')
+                #lostToMiniBoss()
+                quit()
+        playerClass.level += 1
+        victoryFinalBoss()
+    
+    elif whichBoss == 3:
+        finalBoss = Resler() 
+        while finalBoss.hp > 0:
+            #os.system('clear')
+            miniBossFight(playerClass.hp,finalBoss.hp,playerClass.level)
+            selectionNum = input()
+            if selectionNum == '1':
+                finalBoss.hp = finalBoss.hp - playerClass.attack
+            playerClass.hp = playerClass.hp - finalBoss.attack
+            if selectionNum == '2':
+                finalBoss.hp = finalBoss.hp - playerClass.diningDollars
+                playerClass.diningDollars -= 50
+            if playerClass.hp <= 0:
+                #os.system('clear')
+                #lostToMiniBoss()
+                quit()
+        playerClass.level += 1
+        victoryFinalBoss()
+    
+    else whichBoss == 4:
+        finalBoss = Ghosh() 
+        while finalBoss.hp > 0:
+            #os.system('clear')
+            miniBossFight(playerClass.hp,finalBoss.hp,playerClass.level)
+            selectionNum = input()
+            if selectionNum == '1':
+                finalBoss.hp = finalBoss.hp - playerClass.attack
+            playerClass.hp = playerClass.hp - finalBoss.attack
+            if selectionNum == '2':
+                finalBoss.hp = finalBoss.hp - playerClass.diningDollars
+                playerClass.diningDollars -= 50
+            if playerClass.hp <= 0:
+                #os.system('clear')
+                #lostToMiniBoss()
+                quit()
+        playerClass.level += 1
+        victoryFinalBoss()
+
